431738ca66eafd020973fdaf51bff7ba0c01a5a2
unknown <camila.acosta-v@uniminuto.edu.co>
2024-05-01 11:38:12 -0500

    se agrega la funcionalidad a preinscripcion

---
+++ php/taller.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "funciona?";
+// Lee el contenido del archivo HTML
+//$html_content = file_get_contents("../html/taller.html");
+
+// Imprime el contenido en la respuesta HTTP
+//echo $html_content;
+
+
+?>
