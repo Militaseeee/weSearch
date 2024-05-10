88435fa84d29173a76f17361dc5782b393546d29
xXCpFireXx <suarezalejo26@gmail.com>
2024-05-10 08:32:57 -0500

    Se finaliza todo los pasos de proceso

--- js/proceso.js
+++ js/proceso.js
@@ -12,10 +12,43 @@ function verificarFase() {
                 window.location.href = 'taller.html';; // Redirigir a la siguiente fase (hojaVida.html)
             } else if (faseCompletada === '2') {
                 window.location.href = 'hojaVida.html'; // Redirigir a la siguiente fase (hojaVida.html)
-            } // Puedes agregar más casos según las fases que tengas
+            } else if (faseCompletada === '3') {
+                Toastify({
+                    text: 'Ya completaste tus primeros PASOS en el proceso de la practica, es hora de ver algunas OFERTAS',
+                    duration: 5000, // Duración del toast en milisegundos (5 segundos en este caso)
+                    gravity: 'top', // Posición del toast (arriba)
+                    position: 'center', // Posición horizontal del toast (centrado)
+                    backgroundColor: 'radial-gradient(circle, rgba(34,193,195,1) 0%, rgba(45,253,51,1) 51%)',
+                }).showToast();
+            }
         } else {
             console.error('Error al comprobar estado de completado.');
         }
     };
     xhr.send();
+}
+
+function siguiente() {
+    // Obtener el estado del checkbox
+    const completado = document.getElementById('completado').checked;
+
+    // Si el checkbox está marcado
+    if (completado) {
+        // Hacer una solicitud AJAX para enviar los datos al servidor
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '../php/actualizar_taller.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // La solicitud se completó con éxito, puedes redirigir al usuario o mostrar algún mensaje
+                console.log('Datos enviados correctamente');
+            }
+        };
+        // Enviar los datos al servidor
+        xhr.send('completado=1');
+        window.location.href = '../html/hojaVida.html';
+    } else {
+        // El checkbox no está marcado, puedes mostrar un mensaje de advertencia
+        console.log('Debes marcar el checkbox antes de continuar');
+    }
 }
